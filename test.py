493bd2dfed36a02ecd662c85e4f2004c5ce614c3
KrzysiekAgi <225970@student.pwr.edu.pl>
2016-06-23 10:06:07 +0200

    Earth is a sphere
    syntax error while returning from function. I'll take care of it later (TBH don't know why there's a mistake right now)

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import math as m
 
 def cart2pol3d(x, y, z, xx, yy, zz): #xx etc. being anthen
-	fi = m.degrees(m.atan2(y-yy, x-xx)) #flat
-	elev = m.degrees(m.atan2(z-zz, m.sqrt((x-xx)**2+(y-yy)**2))) #height
-	return fi, elev
+	r = sqrt((x-xx)**2+(y-yy)**2+(z-zz)**2)
+	az = m.degrees(m.atan2(y-yy, x-xx)) #flat
+	elev = m.degrees(m.acos((z-zz)/r) #height
+	return (az, elev)
 
-print cart2pol3d(4, 6, 10, 1, 2, 5)
+print cart2pol3d(4, 6, 10, -1, -2, 5)
 
