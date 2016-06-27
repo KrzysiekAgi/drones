0e292f125a00288074d65cb5e607aa1329543785
Rafal Kurda <rafalkurda@gmail.com>
2016-06-27 23:04:09 +0200

    refractored for better understanding

--- program.py
+++ program.py
@@ -1,16 +1,28 @@
 import math as m
 
-def az_elev2(fi1, fi2, L1, L2, H):
+
+def azimuth(fi1, fi2, L1, L2, H):
+    numerator = m.sin(L2)
+    denominator = m.cos(fi1) * m.tan(fi2) - m.sin(fi1) * m.cos(L2)
+    azimuth_in_radians = m.atan(numerator / denominator)
+    azimuth_in_degrees = m.degrees(azimuth_in_radians)
+    return azimuth_in_degrees
+
+
+def elevation(fi1, fi2, L1, L2, H):
     '''source: https://en.wikipedia.org/wiki/Azimuth
     https://pl.wikipedia.org/wiki/Ortodroma
-    fi1 - site's latitude, fi2 - drone's latitude, L2 - dron's longitude, L1 - site's longitude being equal to 0 (?), H - drone's hight in km'''
-    az = m.degrees(m.atan(m.sin(L2)/(m.cos(fi1)*m.tan(fi2)-m.sin(fi1)*m.cos(L2) ) ) )
-    arch_len = 111.195*m.degrees(m.acos(m.sin(fi1)*m.sin(fi2)+m.cos(fi1)*m.cos(fi2)*m.cos(L2-L1))) #in km
-    alfa = (360*arch_len)/(2*m.pi*6378.41)
-    #leng = arch length on flat
-    leng = m.sqrt(6378.41**2 + 6378.41**2 -2*6378.41*6378.41*m.cos(alfa) ) #in km
-    elev = m.asin(H/leng)
-    return az, elev
+    fi1 - site's latitude, fi2 - drone's latitude,
+    L2 - dron's longitude,
+    L1 - site's longitude being equal to 0 (?), H - drone's hight in km'''
+    some_radians = m.acos(m.sin(fi1) * m.sin(fi2) + m.cos(fi1) * m.cos(fi2) * m.cos(L2 - L1))
+    arch_len = 111.195 * m.degrees(some_radians)  # in km
+    alfa = (360 * arch_len) / (2 * m.pi * 6378.41)
+    r = 6378.41
+    r_pow = r ** 2
+    leng = m.sqrt(r_pow * 2 - 2 * r_pow * m.cos(alfa))  # in km
+    elev = m.asin(H / leng)
+    return elev
 
 
 def az_elev(S, N, L): 
@@ -23,4 +35,5 @@ def az_elev(S, N, L):
     return elev, az
 
 
-print az_elev2(15, 30, 0, 20, 2)
+print elevation(15, 30, 0, 20, 2)
+print azimuth(15, 30, 0, 20, 2)
