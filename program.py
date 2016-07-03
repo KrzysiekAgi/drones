275c445cff1a7f60b7b617d4bf23c1d714fb270a
KrzysiekAgi <225970@student.pwr.edu.pl>
2016-07-03 12:03:23 +0200

    Add files via upload

--- program.py
+++ program.py
@@ -2,38 +2,25 @@ import math as m
 
 
 def azimuth(lat_an, lat_dr, lon_an, lon_dr):
-    numerator = m.sin(L2)
-    denominator = m.cos(fi1) * m.tan(fi2) - m.sin(fi1) * m.cos(L2)
+    numerator = m.sin(lon_dr)
+    denominator = m.cos(lat_an) * m.tan(lat_dr) - m.sin(lat_an) * m.cos(lon_dr)
     azimuth_in_radians = m.atan(numerator / denominator)
     azimuth_in_degrees = m.degrees(azimuth_in_radians)
     return azimuth_in_degrees
 
 
-def elevation(lat_an, lat_dr, h_drone):
+def elevation(lat_an, lat_dr, h_drone, lon_an, lon_dr):
     '''source: https://en.wikipedia.org/wiki/Azimuth
     https://pl.wikipedia.org/wiki/Ortodroma
     fi1 - site's latitude, fi2 - drone's latitude,
     L2 - dron's longitude,
     L1 - site's longitude being equal to 0 (?), H - drone's hight in km'''
-    some_radians = m.acos(m.sin(fi1) * m.sin(fi2) + m.cos(fi1) * m.cos(fi2) * m.cos(L2 - L1))
+    some_radians = m.acos(m.sin(lat_an) * m.sin(lat_dr) + m.cos(lat_an) * m.cos(lat_dr) * m.cos(lon_dr - lon_an))
     arch_len = 111.195 * m.degrees(some_radians)  # in km
     alfa = (360 * arch_len) / (2 * m.pi * 6378.41)
     r = 6378.41
     r_pow = r ** 2
     leng = m.sqrt(r_pow * 2 - 2 * r_pow * m.cos(alfa))  # in km
-    elev = m.asin(H / leng)
+    elev = m.asin(h_drone / leng)
     return elev
 
-
-def az_elev(S, N, L): 
-    '''S - drone's longitude, N - site's longitude, L - site's latitude
-    source: http://tiij.org/issues/issues/3_2/3_2e.html 
-    I think it works only for geostationary satelite'''	
-    G = S - N
-    elev = m.degrees(m.atan((m.cos(G)*m.cos(L) - 0.1512)/(m.sqrt(1-m.cos(G)*m.cos(G)*m.cos(L)*m.cos(L)))))
-    az = m.degrees(180+m.atan(m.tan(G)/m.sin(L)))
-    return elev, az
-
-
-print elevation(15, 30, 0, 20, 2)
-print azimuth(15, 30, 0, 20, 2)
--- unit_test.py
+++ unit_test.py
@@ -13,36 +13,37 @@ def are_floats_close(a, b, relative_difference):
 
 class Test_azimuth(unittest.TestCase):
     def test_simple_north(self):
-        a = azimuth(0, 1, 0, 0, 0)
+        a = azimuth(0, 1, 0, 0)
         self.assertTrue(are_floats_close(a, 0, 0.01))
 
     def test_simple_west(self):
-        a = azimuth(0, 0, 0, -1, 0)
+        a = azimuth(0, 0, 0, -1)
         self.assertTrue(are_floats_close(a, -90, 0.01)) #dlaczego -90, a nie 270? Azymut liczymy tylko w dodatnich stopniach w prawo, z tego co wiem 
 
     def test_simple_east(self):
-        a = azimuth(0, 0, 0, 1, 0) #dlaczego ta funkcja przyjmuje 5 argumentów? W deklaracji ma 4. To tylko zapomnienie?
+        a = azimuth(0, 0, 0, 1) 
         self.assertTrue(are_floats_close(a, 90, 0.01))
 
     def test_simple_south(self):
-        a = azimuth(0, -1, 0, 0, 0)
-        self.assertTrue(are_float_close(a, 180, 0.01))
+        a = azimuth(0, -1, 0, 0)
+        self.assertTrue(are_floats_close(a, 180, 0.01))
 
     def test_simple_north_west(self):
-        a = azimuth(0, 1, 0, -1, 0)
+        a = azimuth(0, 1, 0, -1)
         self.assertTrue(are_floats_close(a, -45, 0.01))
 
     def test_simple_south_west(self):
-        a = azimuth(0, -1, 0, -1, 0)
+        a = azimuth(0, -1, 0, -1)
         self.assertTrue(are_floats_close(a, -135, 0.01))
 
     def test_simple_north_east(self):
-        a = azimuth(0, 1, 0, 1, 0)
+        a = azimuth(0, 1, 0, 1)
         self.assertTrue(are_floats_close(a, 45, 0.01))
 
     def test_simple_south_east(self):
-        a = azimuth(0, -1, 0, 1, 0)
+        a = azimuth(0, -1, 0, 1)
         self.assertTrue(are_floats_close(a, 135, 0.01))
 
 if __name__ == '__main__':
-unittest.main()
+    unittest.main()
+
