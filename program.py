8c4d3f043954af2bf96f42e10e615520f24d666e
Rafal Kurda <rafalkurda@gmail.com>
2016-07-15 08:47:30 +0200

    added definition for circle dist

--- program.py
+++ program.py
@@ -6,13 +6,27 @@ def azimuth(lat_an, lon_an, lat_dr, lon_dr):
     denominator = m.cos(m.radians(lat_an)) * m.tan(m.radians(lat_dr)) - m.sin(m.radians(lat_an)) * m.cos(m.radians(lon_dr))
     azimuth_in_radians = m.atan2(denominator, numerator)
     azimuth_in_degrees = m.degrees(azimuth_in_radians)
-    if azimuth_in_degrees >= 0: 
+    if azimuth_in_degrees >= 0:
         return azimuth_in_degrees
     else:
         return azimuth_in_degrees + 360
 
 def circle_dist(lat_a, lon_a, lat_b, lon_b):
-    return 324234
+    '''https://en.wikipedia.org/wiki/Great-circle_distance#Computational_formulas
+    korzystam z tego bardziej skomplikowanego wzoru, ale dokadnego dla wszystkich odleglosci'''
+    lat_an_rad = m.radians(lat_a)
+    lat_dr_rad = m.radians(lat_b)
+    lon_diff_rad = m.radians(lon_b - lon_a)
+    num_part_1 = (m.cos(lat_dr_rad) * m.sin(lon_diff_rad))**2
+    num_part_21 = m.cos(lat_an_rad) * m.sin(lat_dr_rad)
+    num_part_22 = m.sin(lat_an_rad) * m.cos(lat_dr_rad) * m.cos(lon_diff_rad)
+    num_part_2sq = (num_part_21 - num_part_22)**2
+    numerator = m.sqrt(num_part_1 + num_part_2sq)
+    denom_1 = m.sin(lat_an_rad) * m.sin(lat_dr_rad)
+    denom_2 = m.cos(lat_an_rad) * m.cos(lat_dr_rad) * m.cos(lon_diff_rad)
+    denominator = denom_1 + denom_2
+    D = m.atan2(numerator, denominator)
+    return D
 
 def elevation(lat_an, lon_an, h_an, lat_dr, lon_dr, h_dr):
     '''https://en.wikipedia.org/wiki/Great-circle_distance#Computational_formulas
