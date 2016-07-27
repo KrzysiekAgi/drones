1cadd9cb4a6b6026a14d2e4ecf658b1d97ae64d1
Rafal Kurda <rafalkurda@gmail.com>
2016-07-27 20:59:57 +0200

    simple uart prog

--- prog.py
+++ prog.py
@@ -1,16 +1,17 @@
-import serial
-import io
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+import usb
+# import serial
+# import io
+# ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+# sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 
-# 
-# sio.write(unicode("$O10S\n"))
-# sio.write(unicode("$O11E\n"))
+# # 
+# # sio.write(unicode("$O10S\n"))
+# # sio.write(unicode("$O11E\n"))
 
 
-sio.write(unicode("$O10D-14300\n"))
-# sio.write(unicode("$O11R\n"))
-sio.flush() # it is buffering. required to get the data out *now*
-hello = sio.readline()
-print(hello)
-ser.close()
+# sio.write(unicode("$O10D-14300\n"))
+# # sio.write(unicode("$O11R\n"))
+# sio.flush() # it is buffering. required to get the data out *now*
+# hello = sio.readline()
+# print(hello)
+# ser.close()
