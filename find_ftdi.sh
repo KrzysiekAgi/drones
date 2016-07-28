9b9649c6c6e0d6e164f034650fff3e08c1b14abd
Rafal Kurda <rafalkurda@gmail.com>
2016-07-28 09:24:08 +0200

    program automatically detects ftdi port and moves

---
+++ find_ftdi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}"
+        devname="$(udevadm info -q name -p $syspath)"
+        [[ "$devname" == "bus/"* ]] && continue
+        eval "$(udevadm info -q property --export -p $syspath)"
+        [[ -z "$ID_SERIAL" ]] && continue
+        if [[ "$ID_SERIAL" == *"FTDI"* ]]
+        then
+            echo "/dev/$devname"
+        fi
+    )
+done
--- prog.py
+++ prog.py
@@ -1,17 +1,24 @@
-import usb
-# import serial
-# import io
-# ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-# sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+import serial
+import io
+import subprocess
+import 
+
+output = subprocess.Popen(["bash", "find_ftdi.sh"], stdout=subprocess.PIPE).communicate()[0]
+# print output
+# improve checking
+if len(output) < 8:
+    raise Exception("no serial device is attached")
+ser = serial.Serial(output.strip(), 9600, timeout=1)
+sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 
 # # 
 # # sio.write(unicode("$O10S\n"))
 # # sio.write(unicode("$O11E\n"))
 
 
-# sio.write(unicode("$O10D-14300\n"))
-# # sio.write(unicode("$O11R\n"))
-# sio.flush() # it is buffering. required to get the data out *now*
-# hello = sio.readline()
-# print(hello)
-# ser.close()
+sio.write(unicode("$O10D-1430\n"))
+# sio.write(unicode("$O11R\n"))
+sio.flush() # it is buffering. required to get the data out *now*
+hello = sio.readline()
+print(hello)
+ser.close()
