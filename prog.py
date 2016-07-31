726f33236a534c7fb81b5a001aef217ec72ef16f
Rafal Kurda <rafalkurda@gmail.com>
2016-07-31 12:04:42 +0200

    refractored and removed unneedef function

--- prog.py
+++ prog.py
@@ -17,19 +17,16 @@ def get_serial_object_for_rotor():
     ser = serial.Serial(device_name, 9600, timeout=1)
     return ser
 
+def move_horizontaly(ser, steps):
+    sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+    sio.write(unicode(move_up_down_msg("10" , steps)))
+    sio.flush()
+
 def prog():
 
     ser = get_serial_object_for_rotor()
-    sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
-    # # sio.write(unicode("$O10S\n"))
-    # # sio.write(unicode("$O11E\n"))
 
-
-    sio.write(unicode(move_up_down_msg("10" , 5000)))
-    # sio.write(unicode("$O11R\n"))
-    sio.flush() # it is buffering. required to get the data out *now*
-    hello = sio.readline()
-    print(hello)
+    move_horizontaly(ser, 10000)
     ser.close()
 
 if __name__ == "__main__":
