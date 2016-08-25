0c49ddfa86a59daf8fa7a6884978722ef98438b8
Rafal Kurda <rafalkurda@gmail.com>
2016-08-25 20:04:27 +0200

    started adding position stabilizer package

--- position_stabilizer.py
+++ position_stabilizer.py
@@ -1,1 +1,26 @@
-kdjfdsf
+import numpy
+import statistics
+
+class position_stabilizer():
+    def __init__(self, accuracy_in_meters, min_measur_num):
+        self.accuracy = accuracy_in_meters
+        self.min_measur_num = min_measur_num
+        self.longitude_measurments = []
+        self.latitude_measurments = []
+
+    def get_position(self):
+        lon = numpy.mean(self.longitude_measurments)
+        lat = numpy.mean(self.latitude_measurments)
+        return {"lat": lat, "lon": lon}
+
+    def add_measurment(self, lat, lon):
+        self.longitude_measurments.append(lon)
+        self.latitude_measurments.append(lat)
+
+    def is_ready(self):
+        lon_dev = numpy.std(self.longitude_measurments)
+        lat_dev = numpy.std(self.latitude_measurments)
+        if len(self.longitude_measurments) > self.min_measur_num:
+            return True
+        else:
+            return False
--- ut_position_stabilizer.py
+++ ut_position_stabilizer.py
@@ -1,1 +1,38 @@
-kdfjsdlkjf
+import position_stabilizer
+import unittest
+
+
+class Test_stabilizer(unittest.TestCase):
+    def test_simple(self):
+        p = position_stabilizer.position_stabilizer(10, 10)
+        self.assertFalse(p.is_ready())
+
+    def test_not_enough_measurments(self):
+        l = {"lat": 0, "lon": 0}
+        p = position_stabilizer.position_stabilizer(10, 10)
+        p.add_measurment(l["lat"], l["lon"])
+        self.assertFalse(p.is_ready())
+
+    def test_enough_measurments(self):
+        l = {"lat": 45.4545, "lon": 134.324234}
+        p = position_stabilizer.position_stabilizer(10, 10)
+
+        for r in range(1,15):
+            p.add_measurment(l["lat"], l["lon"])
+
+        calculated = p.get_position()
+        self.assertTrue(p.is_ready())
+        self.assertAlmostEqual(calculated["lat"], l["lat"])
+        self.assertAlmostEqual(calculated["lon"], l["lon"])
+
+    def test_bad_accuracy(self):
+        l = {"lat": 0, "lon": 0}
+        p = position_stabilizer.position_stabilizer(10, 10)
+
+        for r in range(1,15):
+            p.add_measurment(p, p)
+
+        self.assertFalse(p.is_ready())
+
+if __name__ == '__main__':
+    unittest.main()
