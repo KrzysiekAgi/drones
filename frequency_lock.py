9792c2b3e3ba4dfb924ed23e77ea3ce550aff312
Rafal Kurda <rafalkurda@gmail.com>
2016-08-27 18:42:54 +0200

    implemented frequency lock

--- frequency_lock.py
+++ frequency_lock.py
@@ -1,1 +1,16 @@
-kdfj
+import time
+
+
+class frequency_lock():
+    def __init__(self, period):
+        self.last_action = time.time()
+        self.period = period
+
+    def can_act(self):
+        current = time.time()
+        s_since_last = abs(self.last_action - current)
+        if s_since_last > self.period:
+            self.last_action = current
+            return True
+        else:
+            return False
--- ut_frequency_lock.py
+++ ut_frequency_lock.py
@@ -1,1 +1,22 @@
-lksdf
+import unittest
+from frequency_lock import frequency_lock
+import time
+
+
+class Test_frequency_lock(unittest.TestCase):
+    def test_simple_can(self):
+        l = frequency_lock(0.1)
+        time.sleep(0.2)
+        for i in range(0, 5):
+            self.assertTrue(l.can_act())
+            time.sleep(0.2)
+
+    def test_cant(self):
+        l = frequency_lock(1)
+        time.sleep(0.2)
+        for i in range(0, 5):
+            self.assertFalse(l.can_act())
+            time.sleep(0.1)
+
+if __name__ == '__main__':
+    unittest.main()
