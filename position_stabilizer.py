c95beef9edb5823d1f6c0ec5848a75adb0662c03
Rafal Kurda <rafalkurda@gmail.com>
2016-08-27 17:07:33 +0200

    position stabilizer is working properly

--- position_stabilizer.py
+++ position_stabilizer.py
@@ -1,37 +1,70 @@
 import numpy
 import geography
 
+
+def clac_error_and_pos(measur_list):
+    lon_list, lat_list = get_lon_lat_list(measur_list)
+    # this function will not work near poles
+    lon_dev = numpy.std(lon_list)
+    lat_dev = numpy.std(lat_list)
+    lon = numpy.mean(lon_list)
+    lat = numpy.mean(lat_list)
+    lat_2 = lat + lat_dev
+    lon_2 = lon + lon_dev
+    error_rad = geography.circle_dist(lat, lon, lat_2, lon_2)
+    error_met = error_rad * 6371 * 1000
+    return {"lon": lon, "lat": lat, "err": error_met}
+
+
+def remove_big_errors(measur_list):
+    result = []
+    p_and_e = clac_error_and_pos(measur_list)
+    print p_and_e["err"]
+    for m in measur_list:
+        lat_1 = p_and_e["lat"]
+        lon_1 = p_and_e["lon"]
+        lat_2 = m["lat"]
+        lon_2 = m["lon"]
+        c_dist = geography.circle_dist(lat_1, lon_1, lat_2, lon_2)
+        m_dist = c_dist * 6371 * 1000
+        print str(m_dist) + "\t" + str(p_and_e["err"])
+        err = max(p_and_e["err"], 0.1)
+        if m_dist <= err:
+            result.append(m)
+
+    return result
+
+
+def get_lon_lat_list(measur_list):
+    lon_list = [x["lon"] for x in measur_list]
+    lat_list = [x["lat"] for x in measur_list]
+    return lon_list, lat_list
+
+
 class position_stabilizer():
     def __init__(self, accuracy_in_meters, min_measur_num):
         self.accuracy = accuracy_in_meters
         self.min_measur_num = min_measur_num
-        self.longitude_measurments = []
-        self.latitude_measurments = []
+        self.measur_list = []
 
     def get_position(self):
-        lon = numpy.mean(self.longitude_measurments)
-        lat = numpy.mean(self.latitude_measurments)
+        lon_list, lat_list = get_lon_lat_list(self.measur_list)
+        lon = numpy.mean(lon_list)
+        lat = numpy.mean(lat_list)
         return {"lat": lat, "lon": lon}
 
     def add_measurment(self, lat, lon):
-        self.longitude_measurments.append(lon)
-        self.latitude_measurments.append(lat)
+        self.measur_list.append({"lat": lat, "lon": lon})
 
     def is_ready(self):
-        enough_measurments = len(self.longitude_measurments) > self.min_measur_num
+        enough_measurments = len(self.measur_list) > self.min_measur_num
         if not enough_measurments:
             return False
 
-        lon_dev = numpy.std(self.longitude_measurments)
-        lat_dev = numpy.std(self.latitude_measurments)
-        lon = numpy.mean(self.longitude_measurments)
-        lat = numpy.mean(self.latitude_measurments)
-        lat_2 = lat + lat_dev
-        lon_2 = lon + lon_dev
-        error_rad = geography.circle_dist(lat, lon, lat_2, lon_2)
-        error_met = error_rad * 6371 * 1000
+        self.measur_list = remove_big_errors(self.measur_list)
+        p_and_e = clac_error_and_pos(self.measur_list)
 
-        is_accurate = self.accuracy > error_met
+        is_accurate = self.accuracy > p_and_e["err"]
         if is_accurate:
             return True
         else:
--- ut_position_stabilizer.py
+++ ut_position_stabilizer.py
@@ -1,23 +1,24 @@
-import position_stabilizer
+from position_stabilizer import position_stabilizer, remove_big_errors
+from position_stabilizer import clac_error_and_pos
 import unittest
 
 
 class Test_stabilizer(unittest.TestCase):
     def test_simple(self):
-        p = position_stabilizer.position_stabilizer(10, 10)
+        p = position_stabilizer(10, 10)
         self.assertFalse(p.is_ready())
 
     def test_not_enough_measurments(self):
         l = {"lat": 0, "lon": 0}
-        p = position_stabilizer.position_stabilizer(10, 10)
+        p = position_stabilizer(10, 10)
         p.add_measurment(l["lat"], l["lon"])
         self.assertFalse(p.is_ready())
 
     def test_enough_measurments(self):
         l = {"lat": 45.4545, "lon": 134.324234}
-        p = position_stabilizer.position_stabilizer(10, 10)
+        p = position_stabilizer(10, 10)
 
-        for r in range(1,15):
+        for r in range(1, 15):
             p.add_measurment(l["lat"], l["lon"])
 
         calculated = p.get_position()
@@ -26,13 +27,104 @@ class Test_stabilizer(unittest.TestCase):
         self.assertAlmostEqual(calculated["lon"], l["lon"])
 
     def test_bad_accuracy(self):
-        l = {"lat": 0, "lon": 0}
-        p = position_stabilizer.position_stabilizer(10, 10)
+        p = position_stabilizer(10, 10)
 
-        for r in range(1,15):
+        for r in range(1, 15):
             p.add_measurment(r, r)
 
         self.assertFalse(p.is_ready())
+
+    def test_good_accuracy(self):
+        p = position_stabilizer(10, 10)
+
+        meters_1 = 0.0000089949
+        for r in range(-6, 6):
+            p.add_measurment(r * meters_1, r * meters_1)
+
+        self.assertTrue(p.is_ready())
+
+    def test_sdfsdf_accuracy(self):
+        p = position_stabilizer(10, 10)
+
+        meters_1 = 0.0000089949 * 0.01
+        for r in range(-20, 20):
+            p.add_measurment(r * meters_1, r * meters_1)
+
+        p.add_measurment(90, 90)
+        self.assertTrue(p.is_ready())
+
+
+class Test_remove_big_errors(unittest.TestCase):
+    def test_nothing_removed(self):
+        m_list = []
+
+        for i in range(0, 10):
+            m_list.append({"lat": 0, "lon": 0})
+
+        result = remove_big_errors(m_list)
+        self.assertEqual(m_list, result)
+
+    def test_one_removed(self):
+        m_list = []
+
+        for i in range(0, 10):
+            m_list.append({"lat": 0, "lon": 0})
+
+        m_list_error = m_list[:]
+        m_list_error.append({"lat": 10, "lon": 10})
+        result = remove_big_errors(m_list_error)
+        self.assertEqual(m_list, result)
+
+    def test_simple2(self):
+        m_list = []
+        l = {"lat": 45.4545, "lon": 134.324234}
+
+        for r in range(1, 15):
+            m_list.append(l)
+
+        result = remove_big_errors(m_list)
+        self.assertEqual(m_list, result)
+
+    def test_simple3(self):
+        m_list = [{'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234}]
+
+        result = remove_big_errors(m_list)
+        self.assertEqual(m_list, result)
+
+
+class Test_calc_errors(unittest.TestCase):
+    def test_simple(self):
+        m_list = [{'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234},
+                  {'lat': 45.4545, 'lon': 134.324234}]
+        r = clac_error_and_pos(m_list)
+        self.assertAlmostEqual(r["lat"], 45.4545)
+        self.assertAlmostEqual(r["lon"], 134.324234)
+        self.assertAlmostEqual(r["err"], 0, 8)
 
 if __name__ == '__main__':
     unittest.main()
