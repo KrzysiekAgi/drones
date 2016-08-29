1c4b189d5d573b21aefc7547f02f826705de02a0
Rafal Kurda <rafalkurda@gmail.com>
2016-08-29 21:08:25 +0200

    fixed where to move

--- math_utils/intelligence.py
+++ math_utils/intelligence.py
@@ -1,4 +1,4 @@
-def where_to_move(requested, actual):
+def where_to_move_relative(requested, actual):
     regular = requested - actual
 
     if regular > 180:
@@ -8,3 +8,18 @@ def where_to_move(requested, actual):
         regular =  regular + 360
 
     return regular
+
+
+def where_to_move_absolute(requested, actual):
+    no_minus = actual % 360
+    regular = requested - no_minus
+    print requested
+    print actual
+    print regular
+    if regular > 180:
+        regular = regular - 360
+
+    if regular < -180:
+        regular =  regular + 360
+
+    return actual + regular
--- math_utils/ut_intelligence.py
+++ math_utils/ut_intelligence.py
@@ -1,31 +1,63 @@
 import unittest
-from intelligence import where_to_move
+from intelligence import where_to_move_relative, where_to_move_absolute
 
 
-class Test_where_to_move(unittest.TestCase):
+class Test_where_to_move_relative(unittest.TestCase):
     def test_zero(self):
-        r = where_to_move(0, 0)
+        r = where_to_move_relative(0, 0)
         self.assertAlmostEqual(r, 0, 5)
 
     def test_one(self):
-        r = where_to_move(1, 0)
+        r = where_to_move_relative(1, 0)
         self.assertAlmostEqual(r, 1, 5)
 
     def test_minus_one(self):
-        r = where_to_move(-1, 0)
+        r = where_to_move_relative(-1, 0)
         self.assertAlmostEqual(r, -1, 5)
 
     def test_359(self):
-        r = where_to_move(359, 0)
+        r = where_to_move_relative(359, 0)
         self.assertAlmostEqual(r, -1, 5)
 
     def test_1(self):
-        r = where_to_move(1, 359)
+        r = where_to_move_relative(1, 359)
         self.assertAlmostEqual(r, 2, 5)
 
     def test_180(self):
-        r = where_to_move(90, -90)
+        r = where_to_move_relative(90, -90)
         self.assertAlmostEqual(r, 180, 5)
+
+
+class Test_where_to_move_absolute(unittest.TestCase):
+    def test_zero(self):
+        r = where_to_move_absolute(0, 0)
+        self.assertAlmostEqual(r, 0, 5)
+
+    def test_one(self):
+        r = where_to_move_absolute(1, 0)
+        self.assertAlmostEqual(r, 1, 5)
+
+    def test_minus_one(self):
+        r = where_to_move_absolute(-1, 0)
+        self.assertAlmostEqual(r, -1, 5)
+
+    def test_359(self):
+        r = where_to_move_absolute(359, 0)
+        self.assertAlmostEqual(r, -1, 5)
+
+    def test_1(self):
+        r = where_to_move_absolute(1, 359)
+        self.assertAlmostEqual(r, 361, 5)
+
+    def test_180(self):
+        r = where_to_move_absolute(90, -90)
+        self.assertAlmostEqual(r, -270, 5)
+
+    def test_far_more(self):
+        r = where_to_move_absolute(5, 720)
+        self.assertAlmostEqual(r, 725, 5)
+
 
 if __name__ == '__main__':
     unittest.main()
+
